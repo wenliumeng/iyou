b4ba252fa7d21679dc976e81957e7ad6a60f14ae
seyMour <120465271@qq.com>
2016-03-26 15:02:21 +0800

    FileUtils %20 out of filename

--- src/org/iyou/common/util/FileUtils.java
+++ src/org/iyou/common/util/FileUtils.java
@@ -5,4 +5,30 @@ package org.iyou.common.util;
  * ☞120465271@qq.com☜
  */
 public class FileUtils {
+    public FileUtils() {}
+
+    /**
+     * Out of the filename's %20
+     * @param fileName
+     * @return
+     */
+    public static String formatFileName(String fileName){
+        char[] content = new char[fileName.length()];
+        char[] result = new char[fileName.length()];
+        fileName.getChars(0,fileName.length(),content,0);
+
+        int len = 0;
+        for(int i = 0; i < content.length;i++){
+            if((content[i] == '%') && (content[i+1] == '2') && (content[i+2] == '0'))
+            {
+                result[(len++)] = ' ';
+                i += 2;
+            }
+            else
+            {
+                result[(len++)] = content[i];
+            }
+        }
+        return String.valueOf(result,0,len);
+    }
 }
