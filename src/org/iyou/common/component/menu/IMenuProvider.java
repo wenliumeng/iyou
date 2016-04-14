53b2ea90a87d15331ca8244a994f7abf61e2aed1
seyMour <120465271@qq.com>
2016-04-14 18:11:20 +0800

    IMenuProvider

--- src/org/iyou/common/component/menu/IMenuProvider.java
+++ src/org/iyou/common/component/menu/IMenuProvider.java
@@ -1,14 +1,17 @@
 package org.iyou.common.component.menu;
 
 
+import org.iyou.common.model.Condition;
+import org.iyou.common.model.Order;
+
 import java.util.Collection;
 import java.util.List;
-import java.util.concurrent.locks.Condition;
 
 /**
- * xxx
+ * menu供给器接口
  * Created by seyMour on 2016/4/14.
  * ☞120465271@qq.com☜
  */
-public class IMenuProvider {
+public abstract interface IMenuProvider {
+    public abstract List<IMenu> getMenus(Collection<Condition> paramCollection, Collection<Order> paramCollection1);
 }
