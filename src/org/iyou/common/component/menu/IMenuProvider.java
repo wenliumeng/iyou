456c231f130702366bb3b4e14c1347f0299f2988
seyMour <120465271@qq.com>
2016-04-14 18:02:05 +0800

    Menu class

---
+++ src/org/iyou/common/component/menu/IMenuProvider.java
@@ -0,0 +1,14 @@
+package org.iyou.common.component.menu;
+
+
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.locks.Condition;
+
+/**
+ * xxx
+ * Created by seyMour on 2016/4/14.
+ * ☞120465271@qq.com☜
+ */
+public class IMenuProvider {
+}
